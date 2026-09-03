172c21fb7dcaf9be27603f6fc302303aaf4c6ef0
abugb <abugb@gmail.com>
2026-09-03 19:47:48 -0400

    testing sketchpad

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 import gradio as gr
 import spaces
 import torch
+import numpy as np
 from huggingface_hub import InferenceClient
 from transformers import pipeline
-
-passrint("Hello")
 
 REMOTE_MODEL = "Qwen/Qwen-3.8-27B"
 LOCAL_MODEL = "Qwen/Qwen3-0.6B"
@@ -17,5 +16,14 @@ def greet(n):
     print(zero.device) # <-- 'cuda:0' 🤗
     return f"Hello {zero + n} Tensor"
 
-demo = gr.Interface(fn=greet, inputs=gr.Number(), outputs=gr.Text())
+# researched sketchpad using AI
+def process_drawing(sketch):
+    img = sketch["composite"]
+    return img
+
+demo = gr.Interface(
+    fn=process_drawing, 
+    inputs=gr.Sketchpad(type = "numpy"), 
+    outputs=gr.Image(label="Your Drawing"))
+
 demo.launch()
